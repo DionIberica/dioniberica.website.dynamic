101186b53d0ac6c4ee846381c0a7155fbadad8c3
Ferran Basora <fcsonline@gmail.com>
2017-03-22 21:58:52 +0000

    Add stripe

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Raven = require('raven');
+var stripe = require('stripe')('sk_test_XCQ8tR2I83v6UnxXS4yTAtjP');
 var Cart = require('../lib/cart');
 
 router.all('*', (req, res, next) => {
@@ -30,11 +31,15 @@ router.post('/coupon', (req, res, next) => {
   next();
 });
 
-router.post('/checkout', (req, res, next) => {
+router.post('/checkout', (req, res) => {
+  var token = req.body.stripeToken;
+
   req.cart.setToken(req.body.stripeToken);
   req.cart.setEmail(req.body.stripeEmail);
 
-  next();
+  stripe.charges.retrieve(token, (err, charge) => {
+    res.json(charge);
+  });
 });
 
 router.all('*', (req, res) => {
