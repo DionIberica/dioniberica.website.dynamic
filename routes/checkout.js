a399a3440460c17800daea8494075052ef2439f7
Ferran Basora <fcsonline@gmail.com>
2017-03-23 23:01:13 +0000

    preview

--- routes/checkout.js
+++ routes/checkout.js
@@ -8,7 +8,16 @@ var info_email = process.env.INFO_EMAIL;
 var webmaster_email = process.env.WEBMASTER_EMAIL;
 
 router.get('/preview', function(req, res) {
+  var key = process.env['STRIPE_EN'];
+  var stripe = require('stripe')(key);
 
+  stripe.charges.retrieve("ch_1A0eqlHgbV9vBiCll57e7Qnq", function(err, charge) {
+    res.app.render('preview', charge, function(err, html){
+      if(err) console.log(err);
+
+      res.status(200).send(html);
+    });
+  });
 });
 
 module.exports = router;
